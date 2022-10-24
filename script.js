4c0baf2d0ea22807209ef37d337c17ddcf0a0817
Juan Osorio <osorioj202@gmail.com>
2022-10-23 20:09:46 -0500

    requisitos para iniciar proyecto desde cero con git hub

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
 console.log("hello, world");
+
+// requisitos para iniciar proyecto desde cero con git hub
+
+// Windows 10@DESKTOP-73VCPVI MINGW64 ~/OneDrive/Desktop/miProyecto
+// $ git@github.com:osorioj202/curso-practico-js-desde-cero.git
+// bash: git@github.com:osorioj202/curso-practico-js-desde-cero.git: No such file or directory
+
+// Windows 10@DESKTOP-73VCPVI MINGW64 ~/OneDrive/Desktop/miProyecto
+// $ git@github.com:osorioj202/curso-practico-js-desde-cero.git
+// bash: git@github.com:osorioj202/curso-practico-js-desde-cero.git: No such file or directory
+
+// Windows 10@DESKTOP-73VCPVI MINGW64 ~/OneDrive/Desktop/miProyecto
+// $ cd ..
+
+// Windows 10@DESKTOP-73VCPVI MINGW64 ~/OneDrive/Desktop
+// $ cd miProyecto
+
+// Windows 10@DESKTOP-73VCPVI MINGW64 ~/OneDrive/Desktop/miProyecto
+// $ git@github.com:osorioj202/curso-practico-js-desde-cero.git
+// bash: git@github.com:osorioj202/curso-practico-js-desde-cero.git: No such file or directory
+
+// Windows 10@DESKTOP-73VCPVI MINGW64 ~/OneDrive/Desktop/miProyecto
+// $ git clone git@github.com:osorioj202/curso-practico-js-desde-cero.git
+// Cloning into 'curso-practico-js-desde-cero'...
+// remote: Enumerating objects: 4, done.
+// remote: Counting objects: 100% (4/4), done.
+// remote: Compressing objects: 100% (3/3), done.
+// remote: Total 4 (delta 0), reused 0 (delta 0), pack-reused 0
+// Receiving objects: 100% (4/4), done.
+
+// Windows 10@DESKTOP-73VCPVI MINGW64 ~/OneDrive/Desktop/miProyecto
+// $ cd curso-practico-js-desde-cero
+
+// Windows 10@DESKTOP-73VCPVI MINGW64 ~/OneDrive/Desktop/miProyecto/curso-practico-js-desde-cero (main)
+// $ ls
+// LICENSE  README.md
+
+// Windows 10@DESKTOP-73VCPVI MINGW64 ~/OneDrive/Desktop/miProyecto/curso-practico-js-desde-cero (main)
+// $ ls -lf
+// ./  ../  .git/  LICENSE  README.md
+
+// Windows 10@DESKTOP-73VCPVI MINGW64 ~/OneDrive/Desktop/miProyecto/curso-practico-js-desde-cero (main)
+// $ git add -A
+
+// Windows 10@DESKTOP-73VCPVI MINGW64 ~/OneDrive/Desktop/miProyecto/curso-practico-js-desde-cero (main)
+// $ git commit -m "hello, world"
+// [main c90f456] hello, world
+//  1 file changed, 1 insertion(+)
+//  create mode 100644 script.js
+
+// Windows 10@DESKTOP-73VCPVI MINGW64 ~/OneDrive/Desktop/miProyecto/curso-practico-js-desde-cero (main)
+// $ git push
+// Enumerating objects: 4, done.
+// Counting objects: 100% (4/4), done.
+// Delta compression using up to 2 threads
+// Compressing objects: 100% (2/2), done.
+// Writing objects: 100% (3/3), 338 bytes | 48.00 KiB/s, done.
+// Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
+// To github.com:osorioj202/curso-practico-js-desde-cero.git
+//    66be6ab..c90f456  main -> main
+
+console.log("requisitos para iniciar proyecto desde cero con git hub");
